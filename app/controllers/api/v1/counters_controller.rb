96c43488b359216f4d2b7e28be3a7ed04ce9c739
Omar De Leo <omar.deleo@gmail.com>
2021-03-10 16:25:47 -0500

    Update Counter#show route

--- app/controllers/api/v1/counters_controller.rb
+++ app/controllers/api/v1/counters_controller.rb
@@ -1,19 +1,24 @@
+require 'date'
+
 class Api::V1::CountersController < ApplicationController
   before_action :set_counter, only: %i[ show update destroy ]
 
   def show
-    @counter.count += 1
-    @counter.save
-    render json: @counter
+    if @counter
+      @counter.count += 1
+      @counter.save
+    else
+      @counter = Counter.create(count: 1)
+    end
+    response = "Rails server running on port 8080. Pinged #{@counter.count} #{@counter.count == 1 ? "time" : "times"}, most recently on #{DateTime.now().strftime("%a, %e %b %Y %H:%M:%S %z")}."
+    render json: {response: response}
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_counter
     @counter = Counter.all.first
   end
 
-  # Only allow a list of trusted parameters through.
   def counter_params
     params.require(:counter).permit(:count)
   end
